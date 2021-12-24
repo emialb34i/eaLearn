818fe73579786365afa8e528be796715662e4622
Emilio Alberini <emilio.alberini@gmail.com>
2021-12-24 19:38:21 +0100

    added lasso and ridge

--- eaLearn/__init__.py
+++ eaLearn/__init__.py
@@ -1,1 +1,3 @@
 from .linear_regression import LinearRegression
+from .linear_regression import Lasso
+from .linear_regression import Ridge
--- eaLearn/linear_regression.py
+++ eaLearn/linear_regression.py
@@ -11,6 +11,29 @@ Implementation of linear regression algortihms.
 
 """
 
+class l1_regularization:
+
+    def __init__(self, lam):
+        self.lam = lam
+
+    def __call__(self, beta):
+        return np.linalg.norm(beta, ord=1) * self.lam
+
+    def grad(self, beta):
+        return self.lam * np.sign(beta)
+
+class l2_regularization:
+
+    def __init__(self, lam):
+        self.lam = lam
+    
+    def __call__(self, beta):
+        return 0.5 * self.lam * np.linalg.norm(beta, ord=2) 
+    
+    def grad(self, beta):
+        return self.lam * beta
+   
+
 class Regression:
 
     def __init__(self, alpha=0.001, fit_intercept=True, max_iter=100):
@@ -23,20 +46,23 @@ class Regression:
         if self.fit_intercept:
             ones = np.ones(len(X)).reshape(len(X), 1)
             X = np.concatenate((ones, X), axis=1)
+
+        self.X = np.array(X)
+        self.y = np.array(y)
+        self.N, self.D = self.X.shape
         
-        # init betas randomly between [-1/N, 1/N]
-        N = X.shape[1]
-        self.beta_hats = np.random.uniform(-1/N, 1/N, size=(N, ))
+        # init betas randomly between [-1/D, 1/D]
+        self.beta_hats = np.random.uniform(-1/self.D, 1/self.D, size=(self.D, ))
 
         # gradient descent on squared loss
         self.training_errors = []
         for i in range(self.max_iter):
-            y_pred = self.beta_hats@self.X
+            y_pred = self.X@self.beta_hats
             # mean squared error
-            mse = np.mean(0.5*(y_pred-y)**2)
+            mse = np.mean((y_pred-self.y)**2 + self.regualiraztion(self.beta_hats))
             self.training_errors.append(mse)
             # grad of mse wrt to betas
-            grad = ((y_pred-y)@X)/len(y)
+            grad = ((y_pred-self.y)@X) + self.regualiraztion.grad(self.beta_hats)
             # gradient descent step
             self.beta_hats = self.beta_hats - self.alpha*grad
 
@@ -66,7 +92,7 @@ class LinearRegression(Regression):
         self.y = np.array(y)
         self.N, self.D = self.X.shape
         
-        # estimate params (closeed form)
+        # estimate params (closed form)
         XtX_inverse = np.linalg.inv(self.X.T@self.X)
         Xty = self.X.T@self.y
         self.beta_hats = XtX_inverse@Xty
@@ -80,8 +106,17 @@ class LinearRegression(Regression):
 
 
 class Lasso(Regression):
-    # TODO: add L1 regularaziation
-    pass
+
+    def __init__(self, lam=1.0, alpha=0.001, fit_intercept=True, max_iter=100):
+        self.regualiraztion = l1_regularization(lam)
+        super(Lasso, self).__init__(alpha, fit_intercept, max_iter)
+
+class Ridge(Regression):
+
+    def __init__(self, lam=1.0, alpha=0.001, fit_intercept=True, max_iter=100):
+        self.regualiraztion = l2_regularization(lam)
+        super(Ridge, self).__init__(alpha, fit_intercept, max_iter)
+    
         
 
 
