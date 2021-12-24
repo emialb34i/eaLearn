f47d3d51eaf5ae5b6cd72a453cf3887278a71d3f
Emilio Alberini <emilioalberini@Emilios-MBP-2.homenet.telecomitalia.it>
2021-12-24 16:01:55 +0100

    refactored regression inherirtance

--- eaLearn/linear_regression.py
+++ eaLearn/linear_regression.py
@@ -13,17 +13,52 @@ Implementation of linear regression algortihms.
 
 class Regression:
 
-    def __init__():
+    def __init__(self, alpha=0.001, fit_intercept=True, max_iter=100):
+        self.max_iter = max_iter
+        self.alpha = alpha
+        self.fit_intercept = fit_intercept
+    
+    def fit(self, X, y):
+        # if we data does not have intercept we add it
+        if self.fit_intercept:
+            ones = np.ones(len(X)).reshape(len(X), 1)
+            X = np.concatenate((ones, X), axis=1)
+        
+        # init betas randomly between [-1/N, 1/N]
+        N = X.shape[1]
+        self.beta_hats = np.random.uniform(-1/N, 1/N, size=(N, ))
+
+        # gradient descent on squared loss
+        self.training_errors = []
+        for i in range(self.max_iter):
+            y_pred = self.beta_hats@self.X
+            # mean squared error
+            mse = np.mean(0.5*(y_pred-y)**2)
+            self.training_errors.append(mse)
+            # grad of mse wrt to betas
+            grad = ((y_pred-y)@X)/len(y)
+            # gradient descent step
+            self.beta_hats = self.beta_hats - self.alpha*grad
+
+    def predict(self, X):
+        # insert intercept
+        if self.fit_intercept:
+            X = np.insert(X, 0, 1, axis=1)
+        self.y_pred = X@self.beta_hats
+        return self.y_pred
+
+
+
+class LinearRegression(Regression):
+
+    def __init__(self, fit_intercept=True):
+        self.fit_intercept = fit_intercept
         pass
 
-
-
-class LinearRegression:
-
-    def fit(self, X, y, fit_intercept=True):
+    def fit(self, X, y):
        
         # if we data does not have intercept we add it
-        if fit_intercept:
+        if self.fit_intercept:
             ones = np.ones(len(X)).reshape(len(X), 1)
             X = np.concatenate((ones, X), axis=1)
 
@@ -31,22 +66,22 @@ class LinearRegression:
         self.y = np.array(y)
         self.N, self.D = self.X.shape
         
-        # estimate params
-        XtX = np.dot(self.X.T, self.X)
-        XtX_inverse = np.linalg.inv(XtX)
-        Xty = np.dot(self.X.T, self.y)
-        self.beta_hats = np.dot(XtX_inverse, Xty)
+        # estimate params (closeed form)
+        XtX_inverse = np.linalg.inv(self.X.T@self.X)
+        Xty = self.X.T@self.y
+        self.beta_hats = XtX_inverse@Xty
 
         # make_predictions
         self.y_hat = np.dot(self.X, self.beta_hats)
 
 
-    def predict(self, X_test, fit_intercept=True):
-        # insert intercept
-        if fit_intercept:
-            X_test = np.insert(X_test, 0, 1, axis=1)
-        self.y_test_hat = np.dot(X_test, self.beta_hats)
-        return self.y_test_hat
+    def predict(self, X):
+        return super(LinearRegression, self).predict(X)
+
+
+class Lasso(Regression):
+    pass
+        
 
 
 
--- examples/example_linear_regression.py
+++ examples/example_linear_regression.py
@@ -11,11 +11,16 @@ from eaLearn.utils.data_manipulation import train_test_split
 
 def main():
 
+    # data
     X, y = make_regression(n_samples=100, n_features=1, noise=20)
     model = LinearRegression()
+    
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
+    
     model.fit(X_train, y_train)
     y_pred_line = model.predict(X)
+
+    print(model.beta_hats)
 
     m1 = plt.scatter(366 * X_train, y_train, s=10)
     m2 = plt.scatter(366 * X_test, y_test, s=10)
