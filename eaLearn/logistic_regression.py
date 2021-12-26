a08ad9ed8b8c026acf660d323df1a271566e55c2
Emilio Alberini <emilio.alberini@gmail.com>
2021-12-27 00:37:40 +0100

    many bug fixes

--- eaLearn/logistic_regression.py
+++ eaLearn/logistic_regression.py
@@ -9,42 +9,37 @@ Logistic Regression
 def sigmoid(X):
     return 1/(1+np.exp(-X))
 
-class LogisticRegression:
+class Regression:
 
-    def __init__(self, alpha=0.001, fit_intercept=True, max_iter=100):
-        self.alpha = alpha
+    def __init__(self, learning_rate=0.01, fit_intercept=True, max_iter=1000):
+        self.learning_rate = learning_rate
         self.fit_intercept = fit_intercept
         self.max_iter = max_iter
-
+    
     def fit(self, X, y):
         # if we data does not have intercept we add it
         if self.fit_intercept:
             ones = np.ones(len(X)).reshape(len(X), 1)
             X = np.concatenate((ones, X), axis=1)
 
-        self.X = np.array(X)
-        self.y = np.array(y)
-        self.N, self.D = self.X.shape
+        # m training examples, n features
+        m, n = self.X.shape
         
-        # init betas randomly
-        self.beta_hats = np.random.randn(self.D, )
+        # init weights randomly
+        self.w = np.random.rand(n)
 
-        # gradient descent on squared loss
-        self.training_errors = []
-        for i in range(self.max_iter):
-            y_pred = sigmoid(self.X@self.beta_hats)
-            # mean squared error
-            mse = np.mean((y_pred-self.y)**2)
-            self.training_errors.append(mse)
-            # grad of mse wrt to betas
-            grad = ((y_pred-self.y)@X)
+        # gradient descent on cross entropy loss
+        for _ in range(self.max_iter):
+            y_pred = sigmoid(X@self.w)
+            # grad of mse wrt to w
+            grad_w = -(y-y_pred)@X*(1/m)
             # gradient descent step
-            self.beta_hats = self.beta_hats - self.alpha*grad
-    
+            self.w = self.w - self.learning_rate*grad_w
+
     def predict(self, X):
         # insert intercept
         if self.fit_intercept:
             X = np.insert(X, 0, 1, axis=1)
-        self.y_pred = X@self.beta_hats
-        return self.y_pred
+        y_pred = np.round(sigmoid(X.dot(self.param))).astype(int)
+        return y_pred
 
--- eaLearn/regression.py
+++ eaLearn/regression.py
@@ -16,43 +16,43 @@ class l1_regularization:
     def __init__(self, lam):
         self.lam = lam
 
-    def __call__(self, beta):
-        return np.linalg.norm(beta, ord=1) * self.lam
+    def __call__(self, w):
+        return np.linalg.norm(w, ord=1) * self.lam
 
-    def grad(self, beta):
-        return self.lam * np.sign(beta)
+    def grad(self, w):
+        return self.lam * np.sign(w)
 
 class l2_regularization:
 
     def __init__(self, lam):
         self.lam = lam
     
-    def __call__(self, beta):
-        return 0.5 * self.lam * np.linalg.norm(beta, ord=2) 
+    def __call__(self, w):
+        return 0.5 * self.lam * np.linalg.norm(w, ord=2) 
     
-    def grad(self, beta):
-        return self.lam * beta
+    def grad(self, w):
+        return self.lam * w
 
 class l1_l2_regularization:
 
     def __init__(self, lam):
         self.lam = lam
 
-    def __call__(self, beta):
-        l1 = np.linalg.norm(beta, ord=1)
-        l2 = 0.5 * np.linalg.norm(beta, ord=2) 
+    def __call__(self, w):
+        l1 = np.linalg.norm(w, ord=1)
+        l2 = 0.5 * np.linalg.norm(w, ord=2) 
         return self.lam * (l1 + l2)
     
-    def grad(self, beta):
-        l1_grad = np.sign(beta)
-        l2_grad = 0.5 * np.linalg.norm(beta, ord=2) 
+    def grad(self, w):
+        l1_grad = np.sign(w)
+        l2_grad = 0.5 * np.linalg.norm(w, ord=2) 
         return self.lam * (l1_grad + l2_grad)
    
 
 class Regression:
 
-    def __init__(self, alpha=0.001, fit_intercept=True, max_iter=100):
-        self.alpha = alpha
+    def __init__(self, learning_rate=0.01, fit_intercept=True, max_iter=1000):
+        self.learning_rate = learning_rate
         self.fit_intercept = fit_intercept
         self.max_iter = max_iter
     
@@ -62,31 +62,30 @@ class Regression:
             ones = np.ones(len(X)).reshape(len(X), 1)
             X = np.concatenate((ones, X), axis=1)
 
-        self.X = np.array(X)
-        self.y = np.array(y)
-        self.N, self.D = self.X.shape
+        # m training examples, n features
+        m, n = self.X.shape
         
-        # init betas randomly
-        self.beta_hats = np.random.randn(self.D, )
+        # init weights randomly
+        self.w = np.random.rand(n)
 
         # gradient descent on squared loss
         self.training_errors = []
-        for i in range(self.max_iter):
-            y_pred = self.X@self.beta_hats
+        for _ in range(self.max_iter):
+            y_pred = X@self.w
             # mean squared error
-            mse = np.mean((y_pred-self.y)**2 + self.regualiraztion(self.beta_hats))
+            mse = np.mean((y-y_pred)**2)
             self.training_errors.append(mse)
-            # grad of mse wrt to betas
-            grad = ((y_pred-self.y)@X) + self.regualiraztion.grad(self.beta_hats)
+            # grad of mse wrt to w
+            grad_w = -((y-y_pred)@X)*(2/m)
             # gradient descent step
-            self.beta_hats = self.beta_hats - self.alpha*grad
+            self.w = self.w - self.learning_rate*grad_w
 
     def predict(self, X):
         # insert intercept
         if self.fit_intercept:
             X = np.insert(X, 0, 1, axis=1)
-        self.y_pred = X@self.beta_hats
-        return self.y_pred
+        y_pred = X@self.w
+        return y_pred
 
 
 
@@ -98,23 +97,15 @@ class LinearRegression(Regression):
 
     def fit(self, X, y):
        
-        # if we data does not have intercept we add it
+        # if the data does not have intercept we add it
         if self.fit_intercept:
             ones = np.ones(len(X)).reshape(len(X), 1)
             X = np.concatenate((ones, X), axis=1)
-
-        self.X = np.array(X)
-        self.y = np.array(y)
-        self.N, self.D = self.X.shape
         
         # estimate params (closed form)
-        XtX_inverse = np.linalg.inv(self.X.T@self.X)
-        Xty = self.X.T@self.y
-        self.beta_hats = XtX_inverse@Xty
-
-        # make_predictions
-        self.y_hat = np.dot(self.X, self.beta_hats)
-
+        XtX_inverse = np.linalg.inv(X.T@X)
+        Xty = X.T@y
+        self.w = XtX_inverse@Xty
 
     def predict(self, X):
         return super(LinearRegression, self).predict(X)
@@ -122,21 +113,21 @@ class LinearRegression(Regression):
 
 class Lasso(Regression):
 
-    def __init__(self, lam=1.0, alpha=0.001, fit_intercept=True, max_iter=100):
+    def __init__(self, lam=1.0, learning_rate=0.001, fit_intercept=True, max_iter=100):
         self.regualiraztion = l1_regularization(lam)
-        super(Lasso, self).__init__(alpha, fit_intercept, max_iter)
+        super(Lasso, self).__init__(learning_rate, fit_intercept, max_iter)
 
 class Ridge(Regression):
 
-    def __init__(self, lam=1.0, alpha=0.001, fit_intercept=True, max_iter=100):
+    def __init__(self, lam=1.0, learning_rate=0.001, fit_intercept=True, max_iter=100):
         self.regualiraztion = l2_regularization(lam)
-        super(Ridge, self).__init__(alpha, fit_intercept, max_iter)
+        super(Ridge, self).__init__(learning_rate, fit_intercept, max_iter)
 
 class ElasticNet(Regression):
 
-    def __init__(self, lam=1.0, alpha=0.001, fit_intercept=True, max_iter=100):
+    def __init__(self, lam=1.0, learning_rate=0.001, fit_intercept=True, max_iter=100):
         self.regualiraztion = l1_l2_regularization(lam)
-        super(ElasticNet, self).__init__(alpha, fit_intercept, max_iter)
+        super(ElasticNet, self).__init__(learning_rate, fit_intercept, max_iter)
     
         
 
